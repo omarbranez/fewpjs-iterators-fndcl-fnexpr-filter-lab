ef390189d63f6268235fb272a1ed04f56ddba51c
Omar <omar.branez@gmail.com>
2021-08-17 23:14:41 -0400

    commit tree 14db4a7780d5b0a7a556ec428e6dee33cde44b8b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr, q) {
+   return arr.filter( el => 
+        el.toLowerCase() === q.toLowerCase()) // case insensitive because we make everything lowercase
+}
+
+function fuzzyMatch(arr, q) { 
+    return arr.filter( el =>
+        el.toLowerCase().indexOf(q.toLowerCase()) === 0 // "1" doesnt return a driver if the beginning matches
+    )
+}
+
+function matchName(arr, q) {
+    return arr.filter( obj => 
+        obj.name === q)
+}
